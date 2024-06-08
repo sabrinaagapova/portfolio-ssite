53022523e4ba1372392f3c1b53a0c8e145f78cf6
Сабрина Агапова <163026513+sabrinaagapova@users.noreply.github.com>
2024-06-08 17:37:31 +0300

    pp

--- send_mail.php
+++ send_mail.php
@@ -3,8 +3,8 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require "PHPMailer-master/src/Exception.php";
-    require "PHPMailer-master/src/PHPMailer.php";
+    require "PHPMailer/src/Exception.php";
+    require "PHPMailer/src/PHPMailer.php";
 
     $mail = new PHPMailer(true);
 	
